10bea8478a7e1a00f81a028511cf9ec5d8bc38a7
mpereira90 <m__pereira@hotmail.com>
2026-05-24 17:33:10 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,10 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#1e293b" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="CasaConta" />
         </Head>
         <body>
           <Main />
